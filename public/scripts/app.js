76483e81c060a67ccb1ba4f10fa120af86267a9e
Marvin Ding <jaegerding@gmail.com>
2021-10-30 17:17:06 -0400

    MVC implemented

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,13 +2,30 @@
 File Name: app.js
 Student's Name: Zhengliang Ding
 Student Id: 301222388
-Date: Oct 9, 2021
-This file is only for testing purposes.
+Date: Oct 30, 2021
 */
 
-(function () {
-    function Start() {
-        console.log("App Started");
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+        //for all the danger button in our project 
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event)=>{
+                if(!confirm("Are you sure?")) 
+                {
+                    event.preventDefault();
+                    window.location.assign('/contact-list');
+                }
+            });
+        }
     }
+
     window.addEventListener("load", Start);
+
 })();
--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -1,96 +1,100 @@
+/*
+File Name: contact.js
+Student's Name: Zhengliang Ding
+Student Id: 301222388
+Date: Oct 30, 2021
+*/
 let express = require('express');
 let router = express.Router();
 
 let mongoose = require('mongoose');
 
-//connect to userModel
+//connect to contact Model
 
 let Contact = require('../models/contacts');
-// let contactController = require('../controllers/contact');
+
+
+let contactController = require('../controllers/contact');
 
 
 /* GET route for contact */
-router.get('/', (req, res, next) =>{
-    Contact.find((err, contactList)=>{
-        if(err){
-            return console.error(err);
-        }else{
-            res.render('contactlist/list', {title: 'Contact List', ContactList:contactList})
-        }
-    });
-});
-
+router.get('/', contactController.displayContactList);
 
 
 /* GET route for contact List page - CREATE Operation*/
-router.get('/add', (req, res, next) =>{ 
-    // router.get('/add', contactController.displayAddPage);
-    res.render('contactlist/add', {title: 'Add a Contact'});
-});
+router.get('/add', contactController.displayAddPage);
+
+// router.get('/add', (req, res, next) =>{ 
+//     res.render('contactlist/add', {title: 'Add a Contact'});
+// });
 
 /* POST route for contact List page - CREATE Operation*/
-router.post('/add', (req, res, next) =>{
-    let newContact = Contact({
-        "contactName": req.body.contactName,
-        "contactNumber": req.body.contactNumber,
-        "emailAddress": req.body.emailAddress
-    });
-    Contact.create(newContact, (err, Contact)=>{
-        if(err){
-            console.log(err);
-            res.end(err);
-        }else{
-            res.redirect('/contact-list');
-        }
-    });
-});
+router.post('/add', contactController.processAddPage);
+// router.post('/add', (req, res, next) =>{
+//     let newContact = Contact({
+//         "contactName": req.body.contactName,
+//         "contactNumber": req.body.contactNumber,
+//         "emailAddress": req.body.emailAddress
+//     });
+//     Contact.create(newContact, (err, Contact)=>{
+//         if(err){
+//             console.log(err);
+//             res.end(err);
+//         }else{
+//             res.redirect('/contact-list');
+//         }
+//     });
+// });
 
 /* GET route for contact List page - UPDATE Operation*/
-router.get('/edit/:id', (req, res, next)=>{
-    let id = req.params.id;
+router.get('/edit/:id', contactController.displayEditPage);
+// router.get('/edit/:id', (req, res, next)=>{
+//     let id = req.params.id;
 
-    Contact.findById(id, (err, contactToEdit)=>{
-        if(err){
-            console.log(err);
-            res.end(err);
-        }else{
-            res.render('contactlist/edit', {title: 'Contact List', contact:contactToEdit})
-        }
-    });
-});
+//     Contact.findById(id, (err, contactToEdit)=>{
+//         if(err){
+//             console.log(err);
+//             res.end(err);
+//         }else{
+//             res.render('contactlist/edit', {title: 'Contact List', contact:contactToEdit})
+//         }
+//     });
+// });
 
 /* POST route for contact List page - UPDATE Operation*/
-router.post('/edit/:id', (req, res, next)=>{
-    let id = req.params.id;
-    let updateContact = Contact({
-        "_id":id,
-        "contactName": req.body.contactName,
-        "contactNumber": req.body.contactNumber,
-        "emailAddress": req.body.emailAddress
-    });
-    Contact.updateOne({_id:id}, updateContact, (err) =>{
-        if(err){
-            console.log(err);
-            res.end(err);
-        }else{
-            res.redirect('/contact-list');
-        }
-    })
-})
+router.post('/edit/:id', contactController.processEditPage);
+// router.post('/edit/:id', (req, res, next)=>{
+//     let id = req.params.id;
+//     let updateContact = Contact({
+//         "_id":id,
+//         "contactName": req.body.contactName,
+//         "contactNumber": req.body.contactNumber,
+//         "emailAddress": req.body.emailAddress
+//     });
+//     Contact.updateOne({_id:id}, updateContact, (err) =>{
+//         if(err){
+//             console.log(err);
+//             res.end(err);
+//         }else{
+//             res.redirect('/contact-list');
+//         }
+//     })
+// })
 
 /* GET route for contact List page - DELETE Operation*/
-router.get('/delete/:id',(req, res, next)=>{
-    let id = req.params.id;
+router.get('/delete/:id', contactController.deleteContact);
+// router.get('/delete/:id',(req, res, next)=>{
+//     let id = req.params.id;
 
-    Contact.remove({_id:id}, (err)=>{
-        if(err){
-            console.log(err);
-            res.end(err);
-        }else{
-            res.redirect('/contact-list');
-        }
-    });
+//     Contact.remove({_id:id}, (err)=>{
+//         if(err){
+//             console.log(err);
+//             res.end(err);
+//         }else{
+//             res.redirect('/contact-list');
+//         }
+//     });
 
-})
+// })
 
 module.exports = router;
